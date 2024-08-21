08d440f517d627f7b08efd4e44f2895b5bd88a0a
sanketPatel8 <sanket.xceptive@gmail.com>
2024-08-21 18:49:55 +0530

    latest change

--- components/tours/TourList4.jsx
+++ components/tours/TourList4.jsx
@@ -18,7 +18,7 @@ import { faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 
-export default function TourList4({heroData}) {
+export default function TourList4({ heroData }) {
   const [sortOption, setSortOption] = useState("");
   const [ddActives, setDdActives] = useState(false);
   const [sidebarActive, setSidebarActive] = useState(false);
@@ -55,11 +55,11 @@ export default function TourList4({heroData}) {
     const fetchData = async () => {
       const sendData = {
         AccessKey: process.env.NEXT_PUBLIC_ACCESS_KEY,
-        start: activeIndex ,
-        type : "",
+        start: activeIndex,
+        type: "",
       };
-      console.log("activeIndex was :" , activeIndex);
-      
+      console.log("activeIndex was :", activeIndex);
+
       try {
         const response = await post("tourlist", sendData);
         setCount(response.Count);
@@ -87,12 +87,9 @@ export default function TourList4({heroData}) {
     fetchData();
   }, [activeIndex]);
 
-  console.log("Tour list data was : " , TourListData);
-  console.log("heroData was : " , heroData);
-  
-  
+  console.log("Tour list data was : ", TourListData);
+  console.log("heroData was : ", heroData);
 
-  
   // pagination
 
   // Callback function to handle index updates
@@ -167,144 +164,144 @@ export default function TourList4({heroData}) {
               </div>
             </div>
 
-            {heroData.Tour_List?.map((elm, ind) => (
-              <div className="row mt-20" key={ind}>
-                <div className="col-12 my-0">
-                  <div className="tourCard -type-2">
-                    <div className="tourCard__image">
-                      <Image
-                        width={420}
-                        height={390}
-                        // src={TourListData?.tour_image}
-                        src="/_next/image?url=%2Fimg%2FtourCards%2F1%2F13.jpeg&w=1080&q=75"
-                        alt="image"
-                      />
+            {/* {heroData.Tour_List?.map((elm, ind) => (
+              <div className="row mt-20" key={ind}></div>
+            ))} */}
 
-                      <button
-                        className={`tourCard__favorite ${
-                          elm?.direct_flight === 0 ? "d-none" : "d-block"
-                        }`}
-                      >
-                        Direct Flight
-                      </button>
-                    </div>
-
-                    <div className="tourCard__content">
-                      <div className="tourCard__location border_yellow">
-                        <FaPersonWalking color="white" size={18} />
-                        zu Kaaba {elm?.distance_to_hotel}
-                      </div>
-
-                      <h3 className="tourCard__title mt-5">
-                        <span>
-                          {elm?.type}-{elm?.name}
-                        </span>
-                      </h3>
-
-                      <div>
-                        <div>
-                          <p className="tourCard__text mt-5 items-center d-flex">
-                            <FaHotel
-                              className="px-1"
-                              color="#dabf4f"
-                              size={25}
-                            />
-                            Hotel: {elm?.hotel_name} ({elm?.hotel_stars}{" "}
-                            <FaStar color="#dabf4f" className="mx-1" />)
-                          </p>
-                        </div>
-
-                        <p className="tourCard__text mt-5">
-                          <FontAwesomeIcon
-                            icon={faQuoteRight}
-                            className="px-1 text-accent-1"
-                          />
-                          {elm.destination}
-                        </p>
-                      </div>
-
-                      <div className="d-flex items-center mt-5">
-                        <div className="d-flex items-center x-gap-5">
-                          <Stars star={elm?.rating} font={12} />
-                        </div>
-
-                        <div className="text-14 ml-10">
-                          <span className="fw-500">{elm?.rating}</span> (
-                          {elm?.rating_count}) - {elm?.company_name}
-                        </div>
-                      </div>
-
-                      <div className="Location">
-                        <span>Departure : {elm?.departures}</span>
-                      </div>
-
-                      <div className="row x-gap-20 y-gap-5 pt-30">
-                        <div className="col-auto">
-                          <div className="d-flex text-14 items-center ">
-                            <FaCalendar color="dabf4f" size={17} />
-                            <p className="mx-1">
-                              {elm?.date_begin} - {elm?.date_end}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="tourCard__info">
-                      <div className="h-60">
-                        <p className="d-flex items-center text-14 p-2 border-info my-2 m_width  ">
-                          <IoTimeOutline
-                            className="mx-2"
-                            color="#DAC04F"
-                            size={20}
-                          />
-                          {elm?.days_of_stay}
-                        </p>
-                        <p
-                          className={`d-flex items-center text-14 bedrooms p-2 border-info my-2 m_width ${
-                            elm?.tour_with_service == null
-                              ? "d-none"
-                              : "d-block"
+            {heroData?.Tour_List?.length === 0 ? (
+              <h2>No tours available</h2>
+            ) : (
+              heroData?.Tour_List?.map((elm, ind) => (
+                <div className="row mt-20" key={ind}>
+                  <div className="col-12 my-0">
+                    <div className="tourCard -type-2">
+                      <div className="tourCard__image">
+                        <Image
+                          width={420}
+                          height={390}
+                          src="/_next/image?url=%2Fimg%2FtourCards%2F1%2F13.jpeg&w=1080&q=75"
+                          alt="image"
+                        />
+                        <button
+                          className={`tourCard__favorite ${
+                            elm?.direct_flight === 0 ? "d-none" : "d-block"
                           }`}
                         >
-                          <MdBed className="mx-2" color="#DAC04F" size={20} />
-                          {elm?.tour_with_service}
-                        </p>
-
-                        <div className="tourCard__price">
-                          <div></div>
-
-                          <div className="d-flex items-center justify-content-center">
-                            <p className="text-20 fw-500 ml-5 text-center">
-                              {elm?.tour_price} €
+                          Direct Flight
+                        </button>
+                      </div>
+                      <div className="tourCard__content">
+                        <div className="tourCard__location border_yellow">
+                          <FaPersonWalking color="white" size={18} />
+                          zu Kaaba {elm?.distance_to_hotel}
+                        </div>
+                        <h3 className="tourCard__title mt-5">
+                          <span>
+                            {elm?.type}-{elm?.name}
+                          </span>
+                        </h3>
+                        <div>
+                          <div>
+                            <p className="tourCard__text mt-5 items-center d-flex">
+                              <FaHotel
+                                className="px-1"
+                                color="#dabf4f"
+                                size={25}
+                              />
+                              Hotel: {elm?.hotel_name} ({elm?.hotel_stars}{" "}
+                              <FaStar color="#dabf4f" className="mx-1" />)
                             </p>
                           </div>
-                          <p className="text-left text-md-center text-lg-center text-xl-center">
-                            including taxes and fee
+                          <p className="tourCard__text mt-5">
+                            <FontAwesomeIcon
+                              icon={faQuoteRight}
+                              className="px-1 text-accent-1"
+                            />
+                            {elm.destination}
                           </p>
                         </div>
+                        <div className="d-flex items-center mt-5">
+                          <div className="d-flex items-center x-gap-5">
+                            <Stars star={elm?.rating} font={12} />
+                          </div>
+                          <div className="text-14 ml-10">
+                            <span className="fw-500">{elm?.rating}</span> (
+                            {elm?.rating_count}) - {elm?.company_name}
+                          </div>
+                        </div>
+                        <div className="Location">
+                          <span>Departure : {elm?.departures}</span>
+                        </div>
+                        <div className="row x-gap-20 y-gap-5 pt-30">
+                          <div className="col-auto">
+                            <div className="d-flex text-14 items-center">
+                              <FaCalendar color="dabf4f" size={17} />
+                              <p className="mx-1">
+                                {elm?.date_begin} - {elm?.date_end}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
                       </div>
-
-                      <button className="button -outline-accent-1 text-accent-1">
-                        <Link href={`/package/${elm?.slug}?id=${elm?.id}`}>
-                          SHOW AVAILABILITY
-                        </Link>
-                      </button>
+                      <div className="tourCard__info">
+                        <div className="h-60">
+                          <p className="d-flex items-center text-14 p-2 border-info my-2 m_width">
+                            <IoTimeOutline
+                              className="mx-2"
+                              color="#DAC04F"
+                              size={20}
+                            />
+                            {elm?.days_of_stay}
+                          </p>
+                          <p
+                            className={`d-flex items-center text-14 bedrooms p-2 border-info my-2 m_width ${
+                              elm?.tour_with_service == null
+                                ? "d-none"
+                                : "d-block"
+                            }`}
+                          >
+                            <MdBed className="mx-2" color="#DAC04F" size={20} />
+                            {elm?.tour_with_service}
+                          </p>
+                          <div className="tourCard__price">
+                            <div></div>
+                            <div className="d-flex items-center justify-content-center">
+                              <p className="text-20 fw-500 ml-5 text-center">
+                                {elm?.tour_price} €
+                              </p>
+                            </div>
+                            <p className="text-left text-md-center text-lg-center text-xl-center">
+                              including taxes and fee
+                            </p>
+                          </div>
+                        </div>
+                        <button className="button -outline-accent-1 text-accent-1">
+                          <Link href={`/package/${elm?.slug}?id=${elm?.id}`}>
+                            SHOW AVAILABILITY
+                          </Link>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))
+            )}
 
-            <div className="d-flex justify-center flex-column mt-60">
-              <Pagination
-                range={AllPage}
-                activeIndex={activeIndex}
-                setActiveIndex={handleIndexChange}
-              />
+            <div
+              className={`${
+                heroData.Tour_List?.length === 0 ? "d-none" : "d-block"
+              }`}
+            >
+              <div className="d-flex justify-center flex-column mt-60">
+                <Pagination
+                  range={AllPage}
+                  activeIndex={activeIndex}
+                  setActiveIndex={handleIndexChange}
+                />
 
-              <div className="text-14 text-center mt-20">
-                Showing results 1-10 of {heroData.Tour_List?.length}
+                <div className="text-14 text-center mt-20">
+                  Showing results 1-10 of {heroData.Tour_List?.length}
+                </div>
               </div>
             </div>
           </div>
