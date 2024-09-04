01ae61e2124b25a7a00c1195f1e7fdb7ea90a243
sanketPatel8 <sanket.xceptive@gmail.com>
2024-09-04 17:27:07 +0530

    commit
    commit

--- app/(pages)/sanket/page.jsx
+++ app/(pages)/sanket/page.jsx
@@ -1,35 +1,9 @@
-"use client";
+import React from 'react'
 
-import Hero1 from "@/components/homes/heros/Hero1";
-import FooterOne from "@/components/layout/footers/FooterOne";
-import FooterTwo from "@/components/layout/footers/FooterTwo";
-import Header1 from "@/components/layout/header/Header1";
-import Sanket from "@/components/sanketbhuva";
-import TourList4 from "@/components/tours/TourList4";
-import React, { useEffect, useState } from "react";
-
-export default function page() {
-  const [heroData, setHeroData] = useState([]);
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  useEffect(() => {
-    console.log("heroData in Page:", heroData); // Log heroData in Page component
-  }, [heroData]);
+const page = () => {
   return (
-    <>
-      <main>
-        <Header1 />
-
-        <div className="mt-50">
-          <Sanket
-            range={5}
-            activeIndex={activeIndex}
-            setActiveIndex={setActiveIndex}
-          />
-        </div>
-
-        {/* <FooterTwo /> */}
-      </main>
-    </>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
