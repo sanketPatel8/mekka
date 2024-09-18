1d51dbc4c6734d5b717397be7150640b2c0b2414
sanketPatel8 <sanket.xceptive@gmail.com>
2024-09-18 16:11:18 +0530

    update
    update

--- components/tourSingle/Included.jsx
+++ components/tourSingle/Included.jsx
@@ -4,12 +4,12 @@ import React from "react";
 export default function Included({PAckageData}) {
   return (
     <div className="row x-gap-130 y-gap-20 pt-20">
-      <div className="col-lg-6">
+      <div className="col-lg-12">
         <div className="row">
           {PAckageData?.Tour_Details?.tour_details?.tour_included?.en_tour_included?.map((elm, i) => (
-            <div key={i} className="d-flex gap-5 col-4">
+            <div key={i} className="d-flex gap-5 col-3 my-3">
               <i className="icon-check flex-center text-10 size-24 rounded-full text-green-2 bg-green-1 mr-15"></i>
-              {elm}
+              <p>{elm}</p>
             </div>
           ))}
         </div>
