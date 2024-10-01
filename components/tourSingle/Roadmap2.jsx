65694339a8a8ac6619cc80810bcb575e47698dc5
sanketPatel8 <sanket.xceptive@gmail.com>
2024-10-01 14:56:29 +0530

    update
    update

--- components/tourSingle/Roadmap2.jsx
+++ components/tourSingle/Roadmap2.jsx
@@ -4,12 +4,15 @@ import { roadmapData2 } from "@/data/tourSingleContent";
 import React, { useEffect, useState } from "react";
 
 export default function RoadMap2({ PAckageData }) {
-  const [activeRoadmap, setActiveRoadmap] = useState(2);
+  const [activeRoadmap, setActiveRoadmap] = useState(0);
   const [RoadMap, setRoadMap] = useState([]);
 
   useEffect(() => {
     setRoadMap(PAckageData?.Tour_Details?.itinerary || []);
   }, [PAckageData]);
+
+  console.log("RoadMap" , RoadMap);
+  
 
   // Combine RoadMap and roadmapData2
   const combinedRoadMap = RoadMap.map((item, index) => ({
