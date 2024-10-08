c662aef87a0f464f2a0a816d2bcbf0dbcabfa586
mallikauptechmind <mallika.uptechmind@gmail.com>
2024-10-08 12:30:01 +0530

    update

--- app/hooks/useAuthRedirect.js
+++ app/hooks/useAuthRedirect.js
@@ -7,16 +7,16 @@ function Useauthredirect() {
 
     const handleRedirect = (status) => {
         if (status === 404) {
-            console.log(status , "404")
             if (authContext.user) {
                 // window.location.href = "/vendor/login";
                 window.location.href = "/partner-login";
             } 
-            else if (authContext.customer) {
-                window.location.href = "/login";
-            }
-        } else if (status === 401 && window.location.pathname !== "/login" && window.location.pathname !== "/partner-login") {
-       
+           
+        } else if (status === 401  && window.location.pathname !== "/partner-login") {
+            if (authContext.user) {
+                // window.location.href = "/vendor/login";
+                window.location.href = "/partner-login";
+            } 
       
         }
 
@@ -25,22 +25,10 @@ function Useauthredirect() {
             window.location.href = "/partner-login";
           }
           
-        // if (!authContext.user) {
-        //     console.log(authContext.user , "user not found")
-        //     window.location.href = "/partner-login";
-        //   }
-        //   if(!authContext.customer) {
-        //     console.log(authContext.customer , "customer not found")
-        //     window.location.href = "/login";
-        //   }
+      
     };
 
-    // const handlePaidRedirect = (isPaid) => {
-    //     if (isPaid === 0) {
-    //         window.location.href = "/vendor/unauthorised";
-    //     }
-    // };
-
+  
 
     return { handleRedirect };
 
