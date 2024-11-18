43c33b63244004e85ce4eac37990d24813d7878f
mallikauptechmind <mallika.uptechmind@gmail.com>
2024-11-18 12:35:44 +0530

    update

--- components/pages/Payment.jsx
+++ components/pages/Payment.jsx
@@ -382,7 +382,7 @@ const handleSecondAmountChange = (e) => {
     const agbAcceptance = document.getElementById("agbAcceptance");
     const item5 = document.getElementById("item5");
     setIsLoading(true)
-    if (selectedCheckbox === 0) {
+    if (selectedCheckbox === 0 && laterPayment == 0) {
       if (!agbAcceptance?.checked || !item5?.checked) {
         showErrorToast(translate, "Please accept terms and conditions");
         setIsLoading(false);
@@ -406,7 +406,7 @@ const handleSecondAmountChange = (e) => {
       }, 3000);
     }
 
-    if (selectedCheckbox === 1) {
+    if (selectedCheckbox === 1 && laterPayment == 0) {
       if (!agbAcceptance.checked || !item5.checked) {
         showErrorToast(translate, "Please accept terms and conditions");
         setIsLoading(false)
@@ -419,7 +419,7 @@ const handleSecondAmountChange = (e) => {
       setAmount(SideBarData?.BookingFild?.SubTotal);
     }
 
-     if (selectedCheckbox === 2 ) {
+     if (selectedCheckbox === 2  && laterPayment == 0) {
       if (
         !firstAmount ||
         !secondAmount ||
@@ -453,6 +453,31 @@ const handleSecondAmountChange = (e) => {
       };
       setBooking(newBooking);
       setAmount(firstAmount);
+    }
+
+    if(laterPayment == 1){
+      if (!agbAcceptance?.checked || !item5?.checked) {
+        showErrorToast(translate, "Please accept terms and conditions");
+        setIsLoading(false);
+        return;
+      }
+      const newBooking = { ...Booking, paymentType: 4 };
+
+      FatchallBooking(newBooking);
+      setTimeout(() => {
+        router.push("#ref");
+        setIsLoading(false)
+
+        setTimeout(() => {
+          localStorage.removeItem("AdultPrice&count");
+          localStorage.removeItem("SelectedPackageHotelNDFlight");
+          localStorage.removeItem("PackageBookingData");
+          localStorage.removeItem("AllAdultsData");
+          localStorage.removeItem("AdditionalServices");
+          localStorage.removeItem("getUserData");
+          localStorage.removeItem("Redirect_Login");
+        }, 10000);
+      }, 3000);
     }
 
    
@@ -985,6 +1010,93 @@ const handleSecondAmountChange = (e) => {
                               </div>
                             </div>
                           </div>
+
+                          <div className="d-flex items-center pointer-check py-3">
+                            <div className="form-checkbox">
+                              <input
+                                type="checkbox"
+                                id="item5"
+                                name="data protection and accept"
+                                // onChange={handleDisabled}
+                              />
+                              <label
+                                htmlFor="item5"
+                                className="form-checkbox__mark"
+                              >
+                                <div className="form-checkbox__icon">
+                                  <svg
+                                    width="10"
+                                    height="8"
+                                    viewBox="0 0 10 8"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M9.29082 0.971021C9.01235 0.692189 8.56018 0.692365 8.28134 0.971021L3.73802 5.51452L1.71871 3.49523C1.43988 3.21639 0.987896 3.21639 0.709063 3.49523C0.430231 3.77406 0.430231 4.22604 0.709063 4.50487L3.23309 7.0289C3.37242 7.16823 3.55512 7.23807 3.73783 7.23807C3.92054 7.23807 4.10341 7.16841 4.24274 7.0289L9.29082 1.98065C9.56965 1.70201 9.56965 1.24984 9.29082 0.971021Z"
+                                      fill="white"
+                                    />
+                                  </svg>
+                                </div>
+                              </label>
+                            </div>
+                            <label htmlFor="item5" className="lh-16 ml-15">
+                              <span className="text-red">* </span>
+                              {translate(
+                                "Yes, I declare my consent to the data protection and accept the Declaration of Consent of mekkabooking GmbH"
+                              )}
+                            </label>
+                          </div>
+
+                          <div className="d-flex items-center pointer-check py-3">
+                            <div className="form-checkbox">
+                              <input
+                                type="checkbox"
+                                id="agbAcceptance"
+                                name="agbAcceptance"
+                                // onChange={handleDisabled}
+                              />
+                              <label
+                                htmlFor="agbAcceptance"
+                                className="form-checkbox__mark"
+                              >
+                                <div className="form-checkbox__icon">
+                                  <svg
+                                    width="10"
+                                    height="8"
+                                    viewBox="0 0 10 8"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M9.29082 0.971021C9.01235 0.692189 8.56018 0.692365 8.28134 0.971021L3.73802 5.51452L1.71871 3.49523C1.43988 3.21639 0.987896 3.21639 0.709063 3.49523C0.430231 3.77406 0.430231 4.22604 0.709063 4.50487L3.23309 7.0289C3.37242 7.16823 3.55512 7.23807 3.73783 7.23807C3.92054 7.23807 4.10341 7.16841 4.24274 7.0289L9.29082 1.98065C9.56965 1.70201 9.56965 1.24984 9.29082 0.971021Z"
+                                      fill="white"
+                                    />
+                                  </svg>
+                                </div>
+                              </label>
+                            </div>
+                            <label htmlFor="agbAcceptance" className="lh-16 ml-15">
+                              <span className="text-red">* </span>
+                              <span>
+                                {translate(
+                                  "I have read the AGB (mekkabooking) and I accept the conditions"
+                                )}
+                              </span>
+                              {/* {companyCode && translate(`. This trip is operated by ${companyCode}.`)} */}
+                              <span className={companyCode ? "d-none" : "d-block"}>
+                                {translate(
+                                  `This trip is operated by ${companyCode}.`
+                                )}
+                              </span>
+                              {/* {companyCode !== null && (
+                                <div >
+                                  {translate(
+                                    `This trip is operated by ${companyCode}.`
+                                  )}
+                                </div>
+                              )} */}
+                            </label>
+                          </div>
                         </div>
                       )
                     }
@@ -1060,9 +1172,13 @@ const handleSecondAmountChange = (e) => {
                         <div className="col-md-3 col-6">
                           <span>{translate("Payment Method")}</span>
                           <div className="text-accent-2">
-                            {selectedCheckbox === 0
-                              ? "Direct Bank Transfer"
-                              : "Online"}
+                         
+
+                              {
+                                laterPayment == 0 ? (selectedCheckbox == 0 ? "Direct Bank Transfer" : "Online") : "Pay Later"
+                              }
+
+                            
                           </div>
                         </div>
                       </div>
