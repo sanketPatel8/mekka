e5294d94106acc17e036559d57834c5c97b89fb0
mallikauptechmind <mallika.uptechmind@gmail.com>
2024-12-02 14:59:21 +0530

    update

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import FooterOne from "@/components/layout/footers/FooterOne";
 import Header1 from "@/components/layout/header/Header1";
@@ -9,65 +9,89 @@ import FooterTwo from "@/components/layout/footers/FooterTwo";
 import { useEffect, useState } from "react";
 import { showErrorToast } from "@/app/utils/tost";
 import { post } from "@/app/utils/api";
-import Head from "next/head";
 
-// export const metadata = {
-//   title: "Blog-single || ViaTour - Travel & Tour React NextJS Template",
-//   description: "ViaTour - Travel & Tour React NextJS Template",
-// };
-
-export default function page({ params }) {
+export default function Page({ params }) {
   const id = params.id;
   const blog = blogs.find((item) => item.id == id) || blogs[0];
 
-  const [BlogData, setBlogData] = useState({})
-  const [Lang, setLang] = useState('')
-  const [Refrash, setRefrash] = useState(false)
-  const [LangChange, setLangChange] = useState('')
+  const [BlogData, setBlogData] = useState({});
+  const [Lang, setLang] = useState('');
+  const [headerLang, setHeaderLang] = useState('');
+  const [descLang, setDescLang] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const sendData = {
-        AccessKey: process.env.NEXT_PUBLIC_ACCESS_KEY,
-        id : id
-      };
-
-      try {
-        const response = await post("blog_details", sendData);
-        setBlogData(response.Blog_Details)
-      } catch (error) {
-        console.error("Error caught:", error);
-        if (
-          error.response &&
-          error.response.data &&
-          error.response.data.message
-        ) {
-          showErrorToast("Please verify your email");
-        } else {
-          showErrorToast("An error occurred during registration.");
-        }
-      }
+  const fetchData = async (locale) => {
+    console.log(locale)
+    const sendData = {
+      AccessKey: process.env.NEXT_PUBLIC_ACCESS_KEY,
+      lang: locale,
+      id: id
     };
 
-    fetchData();
+    try {
+      const response = await post("blog_details", sendData);
+      setBlogData(response.Blog_Details);
+      if (locale == 'EN') {
 
-  }, [Lang]);
+        console.log(locale)
+        setHeaderLang(response.Blog_Details.headOneEn);
+        setDescLang(response.Blog_Details.headOneTextEn);
+        console.log(headerLang, descLang)
+      } else {
+        setHeaderLang(response.Blog_Details.headOne);
+        setDescLang(response.Blog_Details.headOneText);
+        console.log(headerLang, descLang)
+      }
+    } catch (error) {
+      console.error("Error caught:", error);
+      showErrorToast("An error occurred while fetching blog data.");
+    }
+  };
 
+  
   useEffect(() => {
-    const storedLang = typeof window !== "undefined" ? localStorage.getItem("locale") : "";
-    setLang(storedLang);
-  });
- 
+    if (Lang) {
+      console.log("Fetching data for language:", Lang);
+
+      fetchData(Lang);
+    }
+  }, [Lang]);
+  useEffect(() => {
+    const getLocaleFromCookies = () => {
+      const cookies = document.cookie.split(";").reduce((acc, cookie) => {
+        const [key, value] = cookie.split("=");
+        acc[key.trim()] = value;
+        return acc;
+      }, {});
+
+      return cookies.locale || '';
+    };
+
+    const initialLang = getLocaleFromCookies();
+    if (initialLang) {
+      setLang(initialLang);
+    }
+
+    const intervalId = setInterval(() => {
+      const currentLocale = getLocaleFromCookies();
+      if (currentLocale !== Lang) {
+        setLang(currentLocale); 
+        fetchData(currentLocale);
+      }
+    }, 200); 
+
+    return () => clearInterval(intervalId);
+  }, [Lang]); 
+
+  
+
   return (
     <>
-   
       <main>
-        <Header1 setRefrash={setRefrash} />
-     {BlogData?.imageOne && (
-        <Hero1 blog={blog} BlogData={BlogData}   />
-     )}
-        <BlogSingle BlogData={BlogData} Lang={Lang} />
-   
+        <Header1 />
+        {BlogData?.imageOne && (
+          <Hero1 blog={blog} BlogData={BlogData} />
+        )}
+        <BlogSingle BlogData={BlogData} headerLang={headerLang} descLang={descLang} />
         <FooterTwo />
       </main>
     </>
--- components/blogs/BlogSingle.jsx
+++ components/blogs/BlogSingle.jsx
@@ -3,53 +3,31 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 
-export default function BlogSingle({ BlogData, Lang }) {
-  useEffect(() => {
-    console.log("Language changed:", Lang);
-    // You can add any side effects here that need to run when Lang changes
-  }, [Lang]);
+export default function BlogSingle({ BlogData, headerLang,descLang }) {
+ 
 
   return (
     <>
 
-    {Lang === "EN" ? (
-      <section className="layout-pt-md layout-pb-xl">
+<section className="layout-pt-md layout-pb-xl">
         <div className="container">
           <div className="row mt-50 justify-center">
             <div className="col-lg-12">
               <h2 className="text-30 md:text-24">
-                { BlogData?.headOneEn }
+                { headerLang}
               </h2>
               <p
                 className="mt-20"
                 dangerouslySetInnerHTML={{
-                  __html: 
-                     BlogData?.headOneTextEn
-                   
+                  __html: descLang,                        
                 }}
               />
             </div>
           </div>
         </div>
       </section>
-    ) : (
-      <section className="layout-pt-md layout-pb-xl">
-        <div className="container">
-          <div className="row mt-50 justify-center">
-            <div className="col-lg-12">
-              <h2 className="text-30 md:text-24">
-                 { BlogData?.headOne}
-              </h2>
-              <p
-                className="mt-20"
-                dangerouslySetInnerHTML={{
-                  __html:BlogData?.headOneText,}}
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-    )}
+
+   
       
     </>
   );
