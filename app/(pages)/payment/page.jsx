7719fb96b8ee40f797b2fb941427da470b11dece
mallikauptechmind <mallika.uptechmind@gmail.com>
2024-12-19 15:00:09 +0530

    update

--- app/(pages)/payment/page.jsx
+++ app/(pages)/payment/page.jsx
@@ -4,13 +4,20 @@ import FooterOne from "@/components/layout/footers/FooterOne";
 import FooterTwo from "@/components/layout/footers/FooterTwo";
 import Header1 from "@/components/layout/header/Header1";
 import BookingPages from "@/components/pages/BookingPages";
-import Payment from "@/components/pages/Payment";
+// import Payment from "@/components/pages/Payment";
 import React, { useEffect } from "react";
+import dynamic from 'next/dynamic';
+
+const Payment = dynamic(() => import('@/components/pages/Payment'), {
+  ssr: false 
+});
 
 export default function payment() {
   useEffect(() => {
-  
-      document.title = "Payment - MekkaBooking";
+      if (typeof window !== "undefined"){
+
+        document.title = "Payment - MekkaBooking";
+      }
 
   }, []);
   return (
