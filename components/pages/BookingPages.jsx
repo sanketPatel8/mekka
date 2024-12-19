89ff047ab7485c74cf0813012793da0bd18e9a8c
mallikauptechmind <mallika.uptechmind@gmail.com>
2024-12-19 17:19:22 +0530

    update

--- components/pages/BookingPages.jsx
+++ components/pages/BookingPages.jsx
@@ -1444,26 +1444,54 @@ const SubtotalPriceWithAdditional = (type, i) => {
                                 {option.title}
                               </span>
         </div> */}
+        <div className="flex items-center">
 
-<div className="form-checkbox">
+<div className="form-checkbox flex items-center " >
       <input
         type="checkbox"
-        id={`checkbox-${type}-${i}-${idx}`} // Unique ID for the checkbox
+        id={`checkbox-${type}-${i}-${idx}`}
+        name={`checkboxGroup-${type}-${i}-${idx}`}
         checked={formValues[type]?.[i]?.selectedServices?.includes(`${type}-${i}-${idx}-ad-${option.id}-${option.title}`)}
         onChange={(e) => {
           const isChecked = e.target.checked;
+          console.log(isChecked)
           handleCheckboxChange(e, type, i, idx, option.price, option.additional_order, option.title, option.id, isChecked);
         }}
       />
-      <label htmlFor={`checkbox-${type}-${i}-${idx}`} className="form-checkbox__mark">
-        <div className="form-checkbox__icon">
-          <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9.29082 0.971021C9.01235 0.692189 8.56018 0.692365 8.28134 0.971021L3.73802 5.51452L1.71871 3.49523C1.43988 3.21639 0.987896 3.21639 0.709063 3.49523C0.430231 3.77406 0.430231 4.22604 0.709063 4.50487L3.23309 7.0289C3.37242 7.16823 3.55512 7.23807 3.73783 7.23807C3.92054 7.23807 4.10341 7.16841 4.24274 7.0289L9.29082 1.98065C9.56965 1.70201 9.56965 1.24984 9.29082 0.971021Z" fill="white" />
-          </svg>
+      <label
+                                    htmlFor={`checkbox-${type}-${i}-${idx}`}
+                                    className="form-checkbox__mark"
+                                  >
+                                    <div className="form-checkbox__icon">
+                                      <svg
+                                        width="10"
+                                        height="8"
+                                        viewBox="0 0 10 8"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <path
+                                          d="M9.29082 0.971021C9.01235 0.692189 8.56018 0.692365 8.28134 0.971021L3.73802 5.51452L1.71871 3.49523C1.43988 3.21639 0.987896 3.21639 0.709063 3.49523C0.430231 3.77406 0.430231 4.22604 0.709063 4.50487L3.23309 7.0289C3.37242 7.16823 3.55512 7.23807 3.73783 7.23807C3.92054 7.23807 4.10341 7.16841 4.24274 7.0289L9.29082 1.98065C9.56965 1.70201 9.56965 1.24984 9.29082 0.971021Z"
+                                          fill="white"
+                                        />
+                                      </svg>
+                                    </div>
+                                  </label>
+                                  </div>
+                                <label
+                                  htmlFor={`checkbox-${type}-${i}-${idx}`}
+                                  className="lh-16 "
+                                >
+                                  {option.title}
+                                </label>
         </div>
-      </label>
-      <span className="text-14 lh-1 ml-10">{option.title}</span>
-    </div>
+
+      
+        {/* <label htmlFor={`checkbox-${type}-${i}-${idx}`} className="form-checkbox__mark d-flex items-center">
+    
+    <div className="text-14 lh-1 ml-10">{option.title}</div>
+  </label> */}
+
 
 
                         <div className="text-14">
