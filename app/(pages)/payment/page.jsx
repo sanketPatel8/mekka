b9ece1092330f3b774b2cf7fb02ae96180c79a64
mallikauptechmind <mallika.uptechmind@gmail.com>
2024-12-20 09:19:45 +0530

    update

--- app/(pages)/payment/page.jsx
+++ app/(pages)/payment/page.jsx
@@ -1,10 +1,7 @@
 "use client"
 
-import FooterOne from "@/components/layout/footers/FooterOne";
 import FooterTwo from "@/components/layout/footers/FooterTwo";
 import Header1 from "@/components/layout/header/Header1";
-import BookingPages from "@/components/pages/BookingPages";
-// import Payment from "@/components/pages/Payment";
 import React, { useEffect } from "react";
 import dynamic from 'next/dynamic';
 
