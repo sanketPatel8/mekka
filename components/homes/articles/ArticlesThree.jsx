3a495cfd74649a4e20421e83f16f3f5d62356b21
sanketPatel8 <sanket.xceptive@gmail.com>
2024-12-20 14:55:35 +0530

    update
    update

--- components/homes/articles/ArticlesThree.jsx
+++ components/homes/articles/ArticlesThree.jsx
@@ -76,13 +76,13 @@ export default function ArticlesThree() {
         <div
           data-aos="fade-up"
           data-aos-delay=""
-          className="row justify-content-center mb-10 flex-nowrap"
+          className="row justify-content-center mb-10 "
         >
           
-          {ArticalDAta.slice(0, 4).map((elm, i) => (
+          {ArticalDAta.slice(0, 3).map((elm, i) => (
             <div
               key={i}
-              className="col-lg-3 col-md-5 my-lg-5  my-2 border-1 -hover-shadow rounded-12 mx-2 py-10 px-10 shadow"
+              className="col-lg-3 col-md-5 col-12 my-lg-5  my-2 border-1 -hover-shadow rounded-12 mx-2 py-10 px-10 shadow"
             >
               <Link href={`/blog/${elm.id}`} className="blogCard -type-1">
                 <div className="blogCard__image ratio ratio-41:30">
