e03a4bd4288a39f8797174ac42c29582c77eb56e
sanketPatel8 <sanket.xceptive@gmail.com>
2025-01-01 09:47:45 +0530

    update
    update

--- components/layout/components/HeaderMenu.jsx
+++ components/layout/components/HeaderMenu.jsx
@@ -4,8 +4,8 @@ import { useTranslation } from '@/app/context/TranslationContext';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 const HeaderMenu = ({ translate }) => {
-  const pathname = usePathname(); // Get the current pathname
-  const searchParams = useSearchParams(); // Get the current search parameters
+  const pathname = usePathname(); 
+  const searchParams = useSearchParams(); 
 
   return (
     <div className='header-none'>
@@ -16,17 +16,17 @@ const HeaderMenu = ({ translate }) => {
           </Link>
         </div>
         <div>
-          <Link href="/tour?Filtertype=Umrah" className={pathname === "/tour" && searchParams.get("type") === "Umrah" ? "active tab-font-12" : "tab-font-12"}>
+          <Link href="/tour?Filtertype=Umrah" className={pathname === "/tour" && searchParams.get("Filtertype") === "Umrah" ? "active tab-font-12" : "tab-font-12"}>
             {translate("Umrah")}
           </Link>
         </div>
         <div>
-          <Link href="/tour?Filtertype=Hajj" className={pathname === "/tour" && searchParams.get("type") === "Hajj" ? "active tab-font-12" : "tab-font-12"}>
+          <Link href="/tour?Filtertype=Hajj" className={pathname === "/tour" && searchParams.get("Filtertype") === "Hajj" ? "active tab-font-12" : "tab-font-12"}>
             {translate("Hajj")}
           </Link>
         </div>
         <div>
-          <Link href="/tour?Filtertype=Culture Trip" className={pathname === "/tour" && searchParams.get("type") === "Culture Trip" ? "active tab-font-12" : "tab-font-12"}>
+          <Link href="/tour?Filtertype=Culture Trip" className={pathname === "/tour" && searchParams.get("Filtertype") === "Culture Trip" ? "active tab-font-12" : "tab-font-12"}>
             {translate("Culture Trip")}
           </Link>
         </div>
