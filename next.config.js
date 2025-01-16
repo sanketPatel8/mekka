b3dd9a01fb0d9c32c09df2bbeae097d895b837f8
Nikulpipa <133188695+nikulxceptive@users.noreply.github.com>
2025-01-16 11:02:35 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["admin.mekkabooking.com"],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: "xcoder.a2hosted.com",
+      },
+      {
+        hostname: "new-uat.mekkabooking.com",
+      },
+    ],
   },
   reactStrictMode: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = nextConfig;
