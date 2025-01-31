528c31cfd44e7e8405db5edfb9fc65e924d63bc1
MacBook <sanket.xceptive@gmail.com>
2025-01-31 10:09:54 +0530

    update

--- data/mobileMenu.js
+++ data/mobileMenu.js
@@ -1,43 +1,32 @@
 export const menuData = [
   {
     id: 1,
-    label: 'Home',
-    href: '/'
+    label: "Home",
+    href: "/",
   },
   {
     id: 2,
-    label: 'Tour',
-    href: '/tour'
+    label: "Umrah",
+    href: "/tour?Umrahtype=Umrah",
   },
   {
     id: 3,
-    label: 'Hajj',
-    href: '/tour?=Hajj'
+    label: "Hajj",
+    href: "/tour?Hajjtype=Hajj",
   },
   {
     id: 4,
-    label: 'Umrah',
-    href: '/tour?=Umrah'
+    label: "Culture Trip",
+    href: "/tour?Culturetype=Culture+Trip",
   },
-  // {
-  //   id: 5,
-  //   label: "Pages",
-  //   submenu: [
-  //     { id: 51, label: "Destinations", href: "/destinations" },
-  //     { id: 52, label: "About", href: "/about" },
-  //     { id: 53, label: "Contact", href: "/contact" },
-  //     { id: 54, label: "Help center", href: "/help-center" },
-  //     { id: 55, label: "Terms", href: "/terms" },
-  //     { id: 56, label: "Login", href: "/login" },
-  //     { id: 57, label: "Register", href: "/register" },
-  //     { id: 58, label: "404 Page", href: "/404" },
-  //     { id: 59, label: "Invoice", href: "/invoice" },
-  //     { id: 510, label: "UI elements", href: "/ui-elements" },
-  //   ],
-  // },
   {
-    id: 6,
-    label: 'Contact',
-    href: 'contact'
-  }
+    id: 4,
+    label: "Blog",
+    href: "/blogs",
+  },
+  {
+    id: 5,
+    label: "Contact",
+    href: "contact",
+  },
 ];
